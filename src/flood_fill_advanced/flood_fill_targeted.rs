d12cd142689f8961be0179bc8b074efc91adab16
shob <shobsbot@gmail.com>
2025-07-20 03:25:10 +0000

    figured out distance, set distance for nodes

--- src/flood_fill_advanced/flood_fill_targeted.rs
+++ src/flood_fill_advanced/flood_fill_targeted.rs
@@ -7,23 +7,35 @@ use std::collections::VecDeque;
  * distances from the target as it goes
  */
 fn flood_fill_targeted(
-    length: usize,
     width: usize,
-    sr: usize,
+    height: usize,
     sc: usize,
-    tr: usize,
+    sr: usize,
     tc: usize,
+    tr: usize,
     acceptable_difference: i32,
 ) -> Matrix<i32> {
     let mut result_image: Matrix<i32> = Matrix {
-        data: VecDeque::with_capacity(length),
+        data: VecDeque::with_capacity(width * height),
         num_columns: width,
     };
     let mut distance: i32 = 1;
     let directions: [(usize, usize); 3] = [(1, 0), (0, 1), (1, 1)];
+    for col in 0..width {
+        for row in 0..height {
+            let index = result_image.num_columns * col + row;
+            result_image.data[index] = find_distance(col, tc, row, tr);
+        }
+    }
+    result_image
 }
 
-fn find_distance(col1: usize, col2: usize, row1: usize, row2: usize) {
-    let col: isize = col2 as isize - col1 as isize;
-    let row: isize = row2 as isize - row1 as isize;
+fn find_distance(col1: usize, col2: usize, row1: usize, row2: usize) -> i32 {
+    let col: i32 = col2 as i32 - col1 as i32;
+    let row: i32 = row2 as i32 - row1 as i32;
+    let col_sqr: i32 = col * col;
+    let row_sqr: i32 = row * row;
+    let to_be_rooted: i32 = col_sqr + row_sqr;
+    let rooted: i32 = to_be_rooted.isqrt();
+    rooted
 }
